85e27507e85529b9549844f46c57a97f9d8f40b1
samshapiro13 <samshapiro13@gmail.com>
2019-04-17 00:42:41 -0400

    Added filtering by x axis values and animations

--- main.js
+++ main.js
@@ -6,19 +6,12 @@ var height = 600;
 var lastSelectedDot = 5000;
 var circleBorder = d3.rgb("#606060");
 
-const xAxisVals = {
-    ACTMED: "ACT Median",
-    SATAVG: "SAT Average",
-    ADMRATE: "Admission Rate",
-    AVGFACSAL: "Average Faculty Salary",
-    AVGFAMINC: "Average Family Income",
-    MEANEARN8: "Mean Earnings 8 years After Entry",
-    MEDFAMINC: "Median Family Income",
-    MEDDEBTGRAD: "Median Debt on Graduation"
-}
+var xAxisValArr = ["ACT Median", "SAT Average", "Admission Rate", "Average Faculty Salary",
+    "Average Family Income", "Mean Earnings 8 years After Entry", "Median Family Income",
+    "Median Debt on Graduation"]
 
 // Variable to represent the name of the currently selected axis
-var selectedXAxisName = xAxisVals.AVGFAMINC;
+var selectedXAxisName = "Average Family Income";
 
 d3.csv("colleges.csv", function(csv) {
     for (var i = 0; i < csv.length; ++i) {
@@ -73,7 +66,7 @@ d3.csv("colleges.csv", function(csv) {
 
     // Create SVGs and <g> elements as containers for charts
     var chart1G = d3.select("#chart1")
-	                .append("svg:svg")
+                    .append("svg:svg")
 	                .attr("width",width)
 	                .attr("height",height)
                     .append('g');
@@ -101,11 +94,7 @@ d3.csv("colleges.csv", function(csv) {
             return yScale(d["Average Cost"]);
         })
 	    .attr("r", function(d) {
-           if (d[selectedXAxisName] == 0 || d["Average Cost"] == 0) {
-               return 0;
-           } else {
-               return 5;
-           }
+            return (d[selectedXAxisName] == 0 || d["Average Cost"] == 0) ? 0 : 5;
         })
         .on("mouseenter", function(d) {
             d3.select(this).classed('hovered', true);
@@ -134,16 +123,19 @@ d3.csv("colleges.csv", function(csv) {
 
     // Add x-axis
     chart1G
-		.append("g") // create a group node
-		.attr("transform", "translate(0, "+ (height - 30) + ")")
+        .append("g") // create a group node
+        .attr("id", "xaxis")
+        .attr("transform", "translate(0, "+ (height - 30) + ")")
 		.call(selectedXAxis) // call the axis generator
-		.append("text")
+        .append("text")
+        .attr("id", "xaxistitle")
         .attr("fill", "black")
 		.attr("class", "label")
 		.attr("x", width - 16)
 		.attr("y", -6)
 		.style("text-anchor", "end")
-		.text(selectedXAxisName);
+        .text(selectedXAxisName + " (USD)")
+        .attr("font-weight", "bold");
 
     // Add y-axis
     chart1G
@@ -157,5 +149,106 @@ d3.csv("colleges.csv", function(csv) {
 		.attr("y", 6)
 		.attr("dy", ".71em")
 		.style("text-anchor", "end")
-		.text("Average Cost (USD)");
+        .text("Average Cost (USD)")
+        .attr("font-weight", "bold");
+
+
+    // Add dropdown menu
+    var select = d3.select("#wrapper")
+        .append("select")
+        .attr("id", "xaxislabel")
+        .attr("class", "xaxisdropdown")
+        .style("border", "1px solid black")
+        .on('change', onchange)
+
+    // Fill dropdown with colors
+    var options = select
+        .selectAll('option')
+    	.data(xAxisValArr).enter()
+    	.append('option')
+        .text(d => d)
+        .property("selected", function(d) { return d === "Average Family Income"; });
+
+    // Function to change selected color if dropdown selection is changed
+    function onchange() {
+        selectedXAxisName = d3.select("#xaxislabel").property("value");
+        switch (d3.select("#xaxislabel").property("value")) {
+            case "ACT Median":
+                selectedXAxisScale = xScaleAct;
+                selectedXAxis = xAxisAct;
+                break;
+            case "SAT Average":
+                selectedXAxisScale = xScaleSat;
+                selectedXAxis = xAxisSat;
+                break;
+            case "Admission Rate":
+                selectedXAxisScale = xScaleAdm;
+                selectedXAxis = xAxisAdm;
+                break;
+            case "Average Faculty Salary":
+                selectedXAxisScale = xScaleFac;
+                selectedXAxis = xAxisFac;
+                break;
+            case "Average Family Income":
+                selectedXAxisScale = xScaleFamAvg;
+                selectedXAxis = xAxisFamAvg;
+                break;
+            case "Mean Earnings 8 years After Entry":
+                selectedXAxisScale = xScaleMean8;
+                selectedXAxis = xAxisMean8;
+                break;
+            case "Median Family Income":
+                selectedXAxisScale = xScaleFamMed;
+                selectedXAxis = xAxisFamMed;
+                break;
+            case "Median Debt on Graduation":
+                selectedXAxisScale = xScaleDebt;
+                selectedXAxis = xAxisDebt;
+                break;
+            default:
+                selectedXAxisScale = xScaleFamAvg;
+                selectedXAxis = xAxisFamAvg;
+        }
+    }
+
+    var xAxisButton = d3.select("#wrapper")
+        .append("button")
+        .attr("class", "xaxisbutton")
+        .style("border", "1px solid black")
+        .text("Apply New x-Axis")
+        .on("click", function() {
+            // Adjusts x axis and axis label
+            chart1G.select("#xaxis")
+                .transition()
+                .duration(d => 1000)
+                .call(selectedXAxis);
+
+            chart1G.select("#xaxislabel")
+                .transition()
+                .duration(d => 1000)
+                .text(function(d) {
+                    var retName = selectedXAxisName;
+                    if (retName == "ACT Median" || retName == "SAT Average") {
+                            retName += " (Points)";
+                    } else if (retName == "Admission Rate") {
+                        retName += " (%)";
+                    } else {
+                        retName += " (USD)";
+                    }
+                    return retName;
+                });
+
+            // Moves every dot to new spot on adjusted x axis
+            chart1G.selectAll(".dot1")
+                .transition()
+                .duration(function(d) {
+                    return Math.random() * 1300;
+                })
+                .attr('cx', function(d) {
+                    return selectedXAxisScale(d[selectedXAxisName]);
+                })
+                .attr('r', function(d) {
+                    return (d[selectedXAxisName] == 0 || d["Average Cost"] == 0) ? 0 : 5;
+                });
+        });
 });
